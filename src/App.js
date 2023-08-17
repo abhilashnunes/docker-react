a5ea2920ca61d3a0f09dbb335daa569ad8e5913f
Abhilash Nunes <s3667376@student.rmit.edu.au>
2023-08-18 00:48:43 +1000

    updated App file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.....Hello Melbourne!!!!!
+          Edit <code>src/App.js</code> and save to reload.....Hello Austraia!!!!!
         </p>
         <a
           className="App-link"
